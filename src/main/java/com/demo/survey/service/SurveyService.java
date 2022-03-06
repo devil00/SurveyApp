880a967c62d1e5faa3b4d8517790f3d62547c5cb
Mayur Swami <mswami@Mayurs-MacBook-Pro-2.local>
2022-03-07 00:29:42 +0530

    decimal formatting and error handling

--- src/main/java/com/demo/survey/service/SurveyService.java
+++ src/main/java/com/demo/survey/service/SurveyService.java
@@ -9,6 +9,7 @@ import com.demo.survey.models.support.Response;
 import com.demo.survey.models.support.SurveyResult;
 import com.demo.survey.repository.SurveyRepository;
 import com.demo.survey.repository.SurveyResponseRepository;
+import com.demo.survey.util.SurveyConstants;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,6 +17,8 @@ import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -107,9 +110,10 @@ public class SurveyService {
             for (Object[] objects : data) {
                 SurveyResult.AnswerStatistics answerResult = new SurveyResult.AnswerStatistics();
                 answerResult.setAnswerId((Long) objects[0]);
-                double answersCount = (double)objects[1];
+                double answersCount = ((Long)objects[1]).doubleValue();
                 double percentage = (answersCount * 100) / totalResponsesByQuestion;
-                answerResult.setPercentage(percentage);
+                SurveyConstants.DECIMAL_FORMAT.setRoundingMode(RoundingMode.UP);
+                answerResult.setPercentage(Double.valueOf(SurveyConstants.DECIMAL_FORMAT.format(percentage)));
                 surveyResult.getAnswerStatistics().add(answerResult);
             }
         }
